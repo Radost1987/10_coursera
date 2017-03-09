06937fbd77a810923681e6bc8d88c9320eb65333
Юлия Ларионова <yuliya_larionova@inbox.ru>
2017-03-09 10:12:25 +0300

    the name of the variables are changed

--- coursera.py
+++ coursera.py
@@ -5,31 +5,28 @@ from lxml import etree
 from bs4 import BeautifulSoup
 from openpyxl import Workbook
 
-
 def get_courses_list():
     courses_list = []
     base_url = 'https://www.coursera.org/sitemap~www~courses.xml'
-    base_url_content = requests.get(base_url)
-    xml_content = base_url_content.content.translate(None, b'\n')
-    content_of_parse = etree.iterparse(BytesIO(xml_content))
-    for action, element in content_of_parse:
-        if element.text != ' ' and element.text is not None:
-             courses_list.append(element.text)
+    response = requests.get(base_url)
+    xml=response.content.translate(None, b'\n')
+    context = etree.iterparse(BytesIO(xml))
+    for action, elem in context:
+        if elem.text != ' ' and elem.text is not None:
+             courses_list.append(elem.text)
     return courses_list          
-
 
 def get_20_randomly_courses(list_of_courses):
     number_of_courses = 20
     randomly_selected_courses = random.sample(courses_list, number_of_courses)
     return randomly_selected_courses
 
-
 def get_course_info(selected_courses):
     courses_info = []
     for course in randomly_selected_courses:
         course_page = requests.get(course)   
         soup = BeautifulSoup(course_page.content)
-        name = soup.find('div', class_ = "course-title")
+        name = soup.find('h1', class_ = "title display-3-text")
         date = soup.find('div', class_ = "startdate")
         language = soup.find('div', class_ = "language-info")
         rating = soup.find('div', class_ = "ratings-text bt3-visible-xs")
@@ -41,14 +38,13 @@ def get_course_info(selected_courses):
                             ])
     return courses_info
 
-
 def output_courses_info_to_xlsx(filepath, courses_info):
-    xlsx_book = Workbook()
-    courses_info_sheet = xlsx_book.active
-    courses_info_sheet.append(['URL', 'Title', 'Start date', 'Language', 'Rating'])
+    book = Workbook()
+    sheet = book.active
+    sheet.append(['URL', 'Title', 'Start date', 'Language', 'Rating'])
     for course in courses_info:
-        courses_info_sheet.append(course)
-    xlsx_book.save('{}/courses.xlsx'.format(filepath))
+        sheet.append(course)
+    book.save('{}/courses.xlsx'.format(filepath))
 
 
 if __name__ == '__main__':
@@ -56,6 +52,6 @@ if __name__ == '__main__':
     courses_list = get_courses_list()
     randomly_selected_courses = get_20_randomly_courses(courses_list)
     list_of_courses_info = get_course_info(randomly_selected_courses)
-    folder_filepath = input('Enter the path to the folder where you want to save the file xlsx:\n')
+    folder_filepath = input('Введите путь до папки, в которую нужно схранить xlsx файл: ')
     output_courses_info_to_xlsx(folder_filepath, list_of_courses_info)
-    print('Saved')
+    print('Saved courses.xlsx')
